523081f127563e918644e8fe3e674bcf410ee272
Rico Mitchell <ricomitch@icloud.com>
2020-04-10 21:41:50 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,4 +1,4 @@
-class CreateShows < ActiveRecord::Migration[5.2]
+
 
   def change
     create_table :shows do |t|
@@ -8,5 +8,3 @@ class CreateShows < ActiveRecord::Migration[5.2]
       t.integer :rating
     end
   end
-
-end
